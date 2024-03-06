d22d844ea40833a59f835ec9f8b1fd95674e26a0
Ricardo Alvarez <rasoftdev@outlook.com>
2024-03-05 23:13:26 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Auth\AuthController;
 Route::post("login", [AuthController::class, "login"])->name("login");
 
 Route::group([
-    "middleware" => ["jwt.verify"]
+    "middleware" => ["auth:api"]
 ], function () {
     Route::get("me", [AuthController::class, "me"])->name("me");
     Route::get("refresh", [AuthController::class, "refreshToken"])->name("refresh");
