5efa63c815e2ee5be1ef3a080a085600692210c1
Ricardo Alvarez <rasoftdev@outlook.com>
2024-03-16 12:06:42 -0500

    Update 2024_03_16_165542_remove_inputs_persons_table.php

--- database/migrations/2024_03_16_165542_remove_inputs_persons_table.php
+++ database/migrations/2024_03_16_165542_remove_inputs_persons_table.php
@@ -12,8 +12,8 @@ return new class extends Migration {
     {
         Schema::table('persons', function (Blueprint $table) {
             $table->dropColumn('second_name');
-            $table->dropColumn('first_surename');
-            $table->dropColumn('second_surename');
+            $table->dropColumn('first_surname');
+            $table->dropColumn('second_surname');
             $table->string('last_name', 100)->nullable()->after('first_name');
         });
     }
